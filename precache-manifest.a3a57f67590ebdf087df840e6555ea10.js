74195580aa98011213e75185b2fdb2538b1a37fe
Adrian Majewski <andrevfcb@gmail.com>
2020-10-03 12:01:40 +0200

    Updates

---
+++ precache-manifest.a3a57f67590ebdf087df840e6555ea10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f2ae8e6f171335b4227facfe1f5ce2f",
+    "url": "/React_Snake/index.html"
+  },
+  {
+    "revision": "f9b54567845e9fa1ef52",
+    "url": "/React_Snake/static/css/main.78dcefeb.chunk.css"
+  },
+  {
+    "revision": "e09ef6f615e1c4377212",
+    "url": "/React_Snake/static/js/2.c6556089.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Snake/static/js/2.c6556089.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9b54567845e9fa1ef52",
+    "url": "/React_Snake/static/js/main.b035c723.chunk.js"
+  },
+  {
+    "revision": "ed2623d7fb074cdb3ae2",
+    "url": "/React_Snake/static/js/runtime-main.a5f12e2f.js"
+  }
+]);
